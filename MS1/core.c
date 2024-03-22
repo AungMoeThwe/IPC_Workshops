0d702b2de44ef0e2300a0c2c4dde789fe112f13c
Andrew <162043489+AungMoeThwe@users.noreply.github.com>
2024-03-22 01:12:00 -0400

    Add files via upload

---
+++ MS1/core.c
@@ -0,0 +1,34 @@
+/*/////////////////////////////////////////////////////////////////////////
+                        Assignment 1 - Milestone 1
+Full Name  :Aung Moe Thwe
+Student ID#:148396237
+Email      :amthwe@myseneca.ca
+Section    :ZBB
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+/////////////////////////////////////////////////////////////////////////*/
+
+#include<stdio.h>
+#include"core.h"
+
+// As demonstrated in the course notes:
+// https://intro2c.sdds.ca/D-Modularity/input-functions#clearing-the-buffer
+// Clear the standard input buffer
+void clearInputBuffer(void)
+{
+    // Discard all remaining char's from the standard input buffer:
+    while (getchar() != '\n')
+    {
+        ; // do nothing!
+    }
+}
+// Wait for user to input the "enter" key to continue
+void suspend(void)
+{
+    printf("<ENTER> to continue...");
+    clearInputBuffer();
+    putchar('\n');
+}
---
+++ MS1/core.h
@@ -0,0 +1,29 @@
+/*/////////////////////////////////////////////////////////////////////////
+                        Assignment 1 - Milestone 1
+Full Name  :Aung Moe Thwe
+Student ID#:148396237
+Email      :amthwe@myseneca.ca
+Section    :ZBB
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+/////////////////////////////////////////////////////////////////////////*/
+
+
+
+// Clear the standard input buffer
+void clearInputBuffer(void);
+// Wait for user to input the "enter" key to continue
+void suspend(void);
+
+// Functions
+int inputInt(void);
+int inputIntPositive();
+int inputIntRange();
+char inputCharOption();
+
+
+
+
